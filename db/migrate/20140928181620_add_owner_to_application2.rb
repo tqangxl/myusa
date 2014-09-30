039ba746705481c1923143b219857bfc5e6075c2
Chris Papazian <christopher.papazian@gsa.gov>
2014-09-30 15:14:32 -0700

    hack to skip data migration

--- db/migrate/20140928181620_add_owner_to_application2.rb
+++ db/migrate/20140928181620_add_owner_to_application2.rb
@@ -5,13 +5,13 @@ class AddOwnerToApplication2 < ActiveRecord::Migration
     add_column :oauth_applications, :owner_id, :integer, null: true
     add_column :oauth_applications, :owner_type, :string, null: true
 
-    Doorkeeper::Application.reset_column_information
-
-    Doorkeeper::Application.all.each do |a|
-      a.owner_id = Membership.where(oauth_application_id: a.id).first.user_id
-      a.owner_type = 'User'
-      a.save!
-    end
+    # Doorkeeper::Application.reset_column_information
+    #
+    # Doorkeeper::Application.all.each do |a|
+    #   a.owner_id = Membership.where(oauth_application_id: a.id).first.user_id
+    #   a.owner_type = 'User'
+    #   a.save!
+    # end
 
   end
 end
